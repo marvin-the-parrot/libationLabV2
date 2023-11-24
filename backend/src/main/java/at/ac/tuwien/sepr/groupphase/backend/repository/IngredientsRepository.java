38216a78ce75d9714f88987ae2ce1a825abfbe9c
Katarina Pjanic <e1549616@student.tuwien.ac.at>
2023-11-24 23:24:52 +0100

    #34 Adding Entity and Repository of Ingredients

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/IngredientsRepository.java
@@ -0,0 +1,23 @@
+package at.ac.tuwien.sepr.groupphase.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredients;
+import jakarta.transaction.Transactional;
+
+/**
+ * Repository of Ingredients entity.
+ */
+@Repository
+@Transactional
+public interface IngredientsRepository extends JpaRepository<Ingredients, Long> {
+
+    @Query("SELECT i FROM Ingredients i WHERE i.name LIKE %:ingredientsName%")
+    Optional<Ingredients> searchIngredients(@Param("ingredientsName") String ingredientsName);
+
+}
