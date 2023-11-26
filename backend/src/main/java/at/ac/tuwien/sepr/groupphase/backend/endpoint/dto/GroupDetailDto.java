49c4b373a1a8816be5a49141e5910b0b8f2a2530
Valentin Gobl <e11921623@student.tuwien.ac.at>
2023-11-26 23:45:12 +0100

    #37 added GroupDetailDto

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/GroupDetailDto.java
@@ -0,0 +1,22 @@
+package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
+
+public class GroupDetailDto {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
