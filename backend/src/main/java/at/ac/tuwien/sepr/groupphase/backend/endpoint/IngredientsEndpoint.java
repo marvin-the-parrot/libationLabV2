e4eb8e0529c210fd523f9f851eaae5ed8431b307
Katarina Pjanic <e1549616@student.tuwien.ac.at>
2023-11-27 22:15:13 +0100

    #34 Fixing bug for finding more ingredients

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/IngredientsEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/IngredientsEndpoint.java
@@ -1,21 +1,21 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 
-import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredients;
-import at.ac.tuwien.sepr.groupphase.backend.service.IngredientsService;
-
 import java.lang.invoke.MethodHandles;
-import java.util.Optional;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredients;
+import at.ac.tuwien.sepr.groupphase.backend.service.IngredientsService;
+import jakarta.annotation.security.PermitAll;
 
 /**
  * Ingredients endpoint controller.
@@ -30,10 +30,10 @@ public class IngredientsEndpoint {
     @Autowired
     private IngredientsService ingredientsService;
 
-    @Secured("ROLE_USER")
+    @PermitAll
     @RequestMapping(value = "searchIngredients/{ingredientsName}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public Optional<Ingredients> searchIngredients(@PathVariable String ingredientsName) {
+    public List<Ingredients> searchIngredients(@PathVariable String ingredientsName) {
         LOGGER.info("GET " + BASE_PATH + "searchIngredients/{}", ingredientsName);
         return ingredientsService.searchIngredients(ingredientsName);
     }
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/IngredientsRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/IngredientsRepository.java
@@ -1,14 +1,14 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
-import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredients;
-import jakarta.transaction.Transactional;
-
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredients;
+import jakarta.transaction.Transactional;
 
 /**
  * Repository of Ingredients entity.
@@ -18,6 +18,6 @@ import org.springframework.stereotype.Repository;
 public interface IngredientsRepository extends JpaRepository<Ingredients, Long> {
 
     @Query("SELECT i FROM Ingredients i WHERE i.name LIKE %:ingredientsName%")
-    Optional<Ingredients> searchIngredients(@Param("ingredientsName") String ingredientsName);
+    List<Ingredients> searchIngredients(@Param("ingredientsName") String ingredientsName);
 
 }
