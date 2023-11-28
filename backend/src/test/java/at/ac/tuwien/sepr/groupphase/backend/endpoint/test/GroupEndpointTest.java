bdacf307376d13ae8e5b9ee5e568ff8d62d7663e
Katarina Pjanic <e1549616@student.tuwien.ac.at>
2023-11-28 23:17:07 +0100

    #20 JUnit tests for group delete

---
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/test/GroupEndpointTest.java
@@ -0,0 +1,107 @@
+package at.ac.tuwien.sepr.groupphase.backend.endpoint.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.server.ResponseStatusException;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationGroup;
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepr.groupphase.backend.repository.GroupRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.impl.GroupServiceImpl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationGroup;
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepr.groupphase.backend.repository.GroupRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
+
+@SpringBootTest
+@EnableWebMvc
+@WebAppConfiguration
+public class GroupEndpointTest {
+
+	  @Autowired
+	  private WebApplicationContext webAppContext;
+	  private MockMvc mockMvc;
+
+	  @Autowired
+	  private ObjectMapper objectMapper;
+	  
+	  @Autowired
+	  private GroupRepository groupRepository;
+	  
+	  @Autowired
+	  private UserRepository userRepository;
+	  
+	  private ApplicationGroup applicationGroup;
+		
+	  private ApplicationUser applicationUser;
+
+	  @BeforeEach
+	  public void setup() {
+	    this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+		applicationGroup = new ApplicationGroup();
+		applicationGroup.setId(999L);
+		applicationGroup.setName("newGroup");
+		applicationUser = new ApplicationUser();
+		applicationUser.setAdmin(true);
+		applicationUser.setId(999L);
+		applicationUser.setEmail("new@user.at");
+		applicationUser.setName("New user");
+		applicationUser.setPassword("Password");
+		groupRepository.save(applicationGroup);
+		userRepository.save(applicationUser);	
+	  }
+
+	  @Test
+	  @WithMockUser(roles = {"ADMIN"}) 
+	  public void deleteGroup_deleteGroupByExistingIdAndFromHost_expectedFalse() throws Exception {
+		int expected = groupRepository.findAll().size();
+	    mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/groups/{id}/{userId}", applicationGroup.getId(), applicationUser.getId())).andExpect(status().isNoContent());
+		int result = groupRepository.findAll().size();
+	    assertNotEquals(expected, result);
+	  }
+
+	  @Test
+	  @WithMockUser(roles = {"ADMIN"}) 
+	  public void deleteGroup_deleteGroupByNotExistingIdAndFromHost_expectedTrue() throws Exception {
+		int expected = groupRepository.findAll().size();
+	    mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/groups/{id}/{userId}", -60L, applicationUser.getId())).andExpect(status().isNoContent());
+		int result = groupRepository.findAll().size();
+	    assertEquals(expected, result);
+	  }
+	  
+	  @Test
+	  @WithMockUser(roles = {"ADMIN"}) 
+	  public void deleteGroup_deleteGroupByExistingIdAndFromNotHost_expectedTrue() throws Exception {
+		applicationUser.setAdmin(false);
+		userRepository.save(applicationUser);
+		int expected = groupRepository.findAll().size();
+	    mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/groups/{id}/{userId}", applicationGroup.getId(), applicationUser.getId())).andExpect(status().isBadRequest());
+		int result = groupRepository.findAll().size();
+		assertEquals(expected, result);
+	  }
+	  
+}
---
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/service/test/GroupServiceImplTest.java
@@ -0,0 +1,80 @@
+package at.ac.tuwien.sepr.groupphase.backend.service.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.server.ResponseStatusException;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationGroup;
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepr.groupphase.backend.repository.GroupRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.impl.GroupServiceImpl;
+
+@SpringBootTest
+public class GroupServiceImplTest {
+
+	@Autowired
+	private GroupServiceImpl groupServiceImpl;
+	
+	@Autowired
+	private GroupRepository groupRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	private ApplicationGroup applicationGroup;
+	
+	private ApplicationUser applicationUser;
+	
+	@BeforeEach
+	public void setUp() {
+		applicationGroup = new ApplicationGroup();
+		applicationGroup.setId(999L);
+		applicationGroup.setName("newGroup");
+		applicationUser = new ApplicationUser();
+		applicationUser.setAdmin(true);
+		applicationUser.setId(999L);
+		applicationUser.setEmail("new@user.at");
+		applicationUser.setName("New user");
+		applicationUser.setPassword("Password");
+		groupRepository.save(applicationGroup);
+		userRepository.save(applicationUser);	
+	}
+	
+	@Test
+	public void deleteGroup_deleteGroupByExistingIdAndFromHost_expectedFalse() {
+		int expected = groupRepository.findAll().size();
+		groupServiceImpl.deleteGroup(applicationGroup.getId(), applicationUser.getId());
+		int result = groupRepository.findAll().size();
+		
+		assertNotEquals(expected, result);
+	}
+
+	@Test
+	public void deleteGroup_deleteGroupByNotExistingIdAndFromHost_expectedTrue() {
+		int expected = groupRepository.findAll().size();
+		
+		groupServiceImpl.deleteGroup(-60l, applicationUser.getId());
+		int result = groupRepository.findAll().size();
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void deleteGroup_deleteGroupByExistingIdAndNotFromHost_expectedException() {
+		applicationUser.setAdmin(false);
+		userRepository.save(applicationUser);
+
+		ResponseStatusException responseStatusException = assertThrows(ResponseStatusException.class,
+                () -> groupServiceImpl.deleteGroup(applicationGroup.getId(), applicationUser.getId()));
+
+        assertEquals("400 BAD_REQUEST", responseStatusException.getMessage());		
+	}
+	
+}
