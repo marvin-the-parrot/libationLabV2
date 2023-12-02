7f7d20f960c1895d9c0c17ffc696b49e88c634fc
Katarina Pjanic <e1549616@student.tuwien.ac.at>
2023-12-02 00:28:38 +0000

    Feature/delete member of group

---
+++ frontend/src/app/option-dialog/option-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { DialogService } from '../services/dialog.service';
+
+@Component({
+  selector: 'app-option-dialog',
+  templateUrl: './option-dialog.component.html',
+  styleUrls: ['./option-dialog.component.scss']
+})
+export class OptionDialogComponent {
+  
+}
--- frontend/src/app/services/dialog.service.ts
+++ frontend/src/app/services/dialog.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DeleteConfirmationComponent } from '../delete-confirmation/delete-confirmation.component';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { OptionDialogComponent } from '../option-dialog/option-dialog.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DialogService {
+  private selectedOptionSubject = new Subject<string>();
+  private dialogRef: MatDialogRef<OptionDialogComponent>;
+
   constructor(private dialog: MatDialog) {}
 
   openDeleteConfirmation(): Observable<boolean> {
@@ -16,4 +20,13 @@ export class DialogService {
 
     return dialogRef.afterClosed();
   }
+
+  openOptionDialog(): Observable<Boolean> {
+    const dialogRef = this.dialog.open<OptionDialogComponent, any, boolean>(
+      OptionDialogComponent
+    );
+
+    return dialogRef.afterClosed();
+  }
+
 }
