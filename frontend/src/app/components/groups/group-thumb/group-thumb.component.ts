f88301b615cd0bf2b1577deeb60a12a568a21c05
Elias Eckermann <e12119840@student.tuwien.ac.at>
2023-12-04 23:36:58 +0100

    #17 implemented leave button functionality to enable members to leave a group

--- frontend/src/app/components/groups/group-thumb/group-thumb.component.ts
+++ frontend/src/app/components/groups/group-thumb/group-thumb.component.ts
@@ -1,6 +1,11 @@
 import {Component, Input} from '@angular/core';
 import {GroupOverview} from "../../../dtos/group-overview";
-import {UserListGroupDto} from "../../../dtos/user";
+import {UserListDto} from "../../../dtos/user";
+import {DialogService} from "../../../services/dialog.service";
+import {ConfirmationDialogMode} from "../../../confirmation-dialog/confirmation-dialog.component";
+import {GroupsService} from "../../../services/groups.service";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-group-thumb',
@@ -12,4 +17,37 @@ export class GroupThumbComponent {
   // get the group to display from the parent component
   @Input() group: GroupOverview;
   @Input() username: string;
+
+  constructor(
+    private dialogService: DialogService,
+    private service: GroupsService,
+    private router: Router,
+    private notification: ToastrService
+  ) {
+  }
+
+
+  leaveGroup() {
+    const user: UserListDto = JSON.parse(localStorage.getItem('user'));
+    if (user == null) {
+      this.router.navigate(['/login']);
+      return;
+    }
+    this.dialogService.openConfirmationDialog(ConfirmationDialogMode.Leave).subscribe((result) => {
+      if (result) {
+        this.service.removeMemberFromGroup(this.group.id, user.id).subscribe({
+          next: data => {
+            this.notification.success(`Successfully left Group '${this.group.name}'.`);
+            this.router.navigate(['/groups']);
+          },
+          error: error => {
+            console.error(`Error leaving group.`, error);
+            this.notification.error(`Error leaving group.`); // todo: show error message from backend
+          }
+        });
+      } else {
+        this.router.navigate(['/groups']);
+      }
+    });
+  }
 }
--- frontend/src/app/confirmation-dialog/confirmation-dialog.component.ts
+++ frontend/src/app/confirmation-dialog/confirmation-dialog.component.ts
@@ -3,7 +3,8 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 
 export enum ConfirmationDialogMode {
   Delete,
-  MakeHost
+  MakeHost,
+  Leave
   // add more modes here if needed
 }
 
@@ -19,6 +20,7 @@ export class ConfirmationDialogComponent {
 
   dialogHeader = "Are you sure?";
   dialogText = "";
+  backgroundColor = '#f95959';
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { mode: ConfirmationDialogMode }) {
 
@@ -27,11 +29,19 @@ export class ConfirmationDialogComponent {
         this.dialogHeader = 'Confirm remove user'
         this.dialogText = "Are you sure you want to remove this user from the group?"
         this.confirmBtnText = 'Remove user';
+        this.backgroundColor = '#f95959';
         break;
       case ConfirmationDialogMode.MakeHost:
         this.dialogHeader = "Confirm new host";
         this.dialogText = "Are you sure you want to make this user the new host?"
         this.confirmBtnText = 'Make user host';
+        this.backgroundColor = '#ffb743';
+        break;
+      case ConfirmationDialogMode.Leave:
+        this.dialogHeader = "Confirm leave group";
+        this.dialogText = "Are you sure you want to leave this group?"
+        this.confirmBtnText = 'Leave group';
+        this.backgroundColor = '#f95959';
         break;
       default:
         console.error("Invalid confirmation dialog mode");
