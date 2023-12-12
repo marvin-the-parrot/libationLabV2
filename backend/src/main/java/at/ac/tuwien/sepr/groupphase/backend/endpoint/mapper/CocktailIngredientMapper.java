f0754b85e823f61b3615b9c4633fb112eac182ac
Katarina Pjanic <e1549616@student.tuwien.ac.at>
2023-12-12 01:38:31 +0000

    Bug/search cocktail

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/CocktailIngredientMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/CocktailIngredientMapper.java
@@ -1,14 +1,16 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.CocktailOverviewDto;
-import at.ac.tuwien.sepr.groupphase.backend.entity.Cocktail;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.CocktailListDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.CocktailOverviewDto;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Cocktail;
 import at.ac.tuwien.sepr.groupphase.backend.entity.CocktailIngredients;
 
 @Mapper
@@ -18,28 +20,18 @@ public interface CocktailIngredientMapper {
     @Mapping(source = "cocktail.name", target = "name")
     @Mapping(source = "cocktail.imagePath", target = "imagePath")
     default List<CocktailListDto> cocktailIngredientToCocktailListDto(List<CocktailIngredients> cocktailIngredientsList) {
-        List<CocktailListDto> convertedDto = new ArrayList<>();
+        Map<String, CocktailListDto> cocktailMap = new HashMap<>();
         for (CocktailIngredients eachCocktailIngredients : cocktailIngredientsList) {
-            if (!convertedDto.stream().anyMatch(dto -> dto.getName().equals(eachCocktailIngredients.getCocktail().getName()))) {
-                convertedDto.add(new CocktailListDto(eachCocktailIngredients.getCocktail().getId(),
-                      eachCocktailIngredients.getCocktail().getName(), eachCocktailIngredients.getCocktail().getImagePath(), List.of(eachCocktailIngredients.getIngredient().getName())));
-            } else {
-                convertedDto.stream().anyMatch(dto -> {
-                    if (dto.getName().equals(eachCocktailIngredients.getCocktail().getName())) {
-                        List<String> ingredientsNameList = new ArrayList<String>();
-                        String ingredientsName = eachCocktailIngredients.getIngredient().getName();
-                        ingredientsNameList.add(ingredientsName);
-                        ingredientsNameList.addAll(dto.getIngredientsName());
-                        dto.setIngredientsName(ingredientsNameList);
-                    }
-                    return true;
-                });
-            }
+            String cocktailName = eachCocktailIngredients.getCocktail().getName();
+            String ingredientName = eachCocktailIngredients.getIngredient().getName();
+            CocktailListDto dto = cocktailMap.computeIfAbsent(cocktailName, key ->
+                    new CocktailListDto(eachCocktailIngredients.getCocktail().getId(),
+                            cocktailName, eachCocktailIngredients.getCocktail().getImagePath(), new ArrayList<>()));
+            dto.getIngredientsName().add(ingredientName);
         }
-        return convertedDto;
+        return new ArrayList<>(cocktailMap.values());
     }
 
-    ;
-
+    
     CocktailOverviewDto cocktailToCocktailOverviewDto(Cocktail cocktail);
 }
