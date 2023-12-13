0320d1d1115db128535ceb190c99faa541afec46
Elias Eckermann <e12119840@student.tuwien.ac.at>
2023-12-13 23:56:24 +0100

    #72 wrote some tests for methods in CustomUserDetailService and IngredientServiceImpl

---
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/service/test/CustomUserDetailServiceTest.java
@@ -0,0 +1,63 @@
+package at.ac.tuwien.sepr.groupphase.backend.service.test;
+
+import at.ac.tuwien.sepr.groupphase.backend.repository.GroupRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UserGroupRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.impl.CustomUserDetailService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@ActiveProfiles({"test", "generateData"})
+@SpringBootTest
+
+public class CustomUserDetailServiceTest {
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+    private UserGroupRepository userGroupRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void findUsersByGroup_withValidGroup_expectedSuccess() {
+        var group = groupRepository.findById(1L).orElse(null);
+        assertNotNull(group);
+
+        var users = assertDoesNotThrow(() -> customUserDetailService.findUsersByGroup(group));
+        assertEquals(3, users.size());
+        assertEquals("User1", users.get(0).getName());
+        assertEquals("User3", users.get(1).getName());
+        assertEquals("User4", users.get(2).getName());
+    }
+
+    @Test
+    @Transactional
+    public void deleteUserByEmail_withValidEmail_expectedSuccess() {
+        var group = groupRepository.findById(1L).orElse(null);
+        assertNotNull(group);
+        var members = userGroupRepository.findAllByApplicationGroup(group);
+        assertEquals(3, members.size());
+
+        var user = userRepository.findById(1L).orElse(null);
+        assertNotNull(user);
+        assertEquals("User1", user.getName());
+
+        assertDoesNotThrow(() -> customUserDetailService.deleteUserByEmail("user1@email.com"));
+
+        assertNull(userRepository.findById(1L).orElse(null));
+
+        members = userGroupRepository.findAllByApplicationGroup(group);
+        assertEquals(2, members.size());
+    }
+}
--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/service/test/IngredientServiceImplTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/service/test/IngredientServiceImplTest.java
@@ -1,43 +1,69 @@
 package at.ac.tuwien.sepr.groupphase.backend.service.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
+import at.ac.tuwien.sepr.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepr.groupphase.backend.repository.IngredientsRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.impl.IngredientServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredient;
-import at.ac.tuwien.sepr.groupphase.backend.repository.IngredientsRepository;
-import at.ac.tuwien.sepr.groupphase.backend.service.impl.IngredientServiceImpl;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("generateData")
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles({"test", "generateData"})
 @SpringBootTest
 public class IngredientServiceImplTest {
 
-	@Autowired
-	private IngredientsRepository ingredientsRepository;
+    @Autowired
+    private IngredientsRepository ingredientsRepository;
 
-	@Autowired
-	private IngredientServiceImpl ingredientServiceImpl;
+    @Autowired
+    private IngredientServiceImpl ingredientServiceImpl;
 
-	@Test
-	public void findByNameContainingIgnoreCase_searchingForIngredientVery_findingTwoResult() throws JsonProcessingException {
-		int expected = 7;
+    @Test
+    public void findByNameContainingIgnoreCase_searchingForIngredientVery_findingTwoResult() throws JsonProcessingException {
+        int expected = 7;
         int result = ingredientServiceImpl.searchIngredients("Lemon").size();
 
         assertEquals(expected, result);
-	}
+    }
 
-	@Test
-	public void findByNameContainingIgnoreCase_searchingForIngredientYeastWhichIsNotInDb_findingOneResult() throws JsonProcessingException {
-		int expected = 1;
+    @Test
+    public void findByNameContainingIgnoreCase_searchingForIngredientYeastWhichIsNotInDb_findingOneResult() throws JsonProcessingException {
+        int expected = 1;
         int result = ingredientServiceImpl.searchIngredients("Milk").size();
 
         assertEquals(expected, result);
-	}
+    }
+
+    @Test
+    public void searchIngredients_ingredientNotInOurDbReturnsEntryFromApi_expectedSuccess() {
+        var ingredients = assertDoesNotThrow(() -> ingredientServiceImpl.searchIngredients("Onion"));
+        assertEquals(1, ingredients.size());
+        assertEquals("Onion", ingredients.get(0).getName());
+    }
+
+    @Test
+    public void searchIngredients_ingredientNotInOurDbAndNoApiEntry_expectedNull() {
+        var ingredients = assertDoesNotThrow(() -> ingredientServiceImpl.searchIngredients("Baumkuchen"));
+        assertNull(ingredients);
+    }
+
+    @Test
+    public void getAllGroupIngredients_withValidGroupId_expectedSuccess() {
+        var ingredients = assertDoesNotThrow(() -> ingredientServiceImpl.getAllGroupIngredients(1L));
+        assertEquals(86, ingredients.size());
+
+    }
+
+    @Test
+    public void getAllGroupIngredients_withInvalidGroupId_expectedException() {
+        var exception = assertThrows(NotFoundException.class, () -> ingredientServiceImpl.getAllGroupIngredients(100L));
+        assertEquals("Group not found", exception.getMessage());
+    }
 
 }
