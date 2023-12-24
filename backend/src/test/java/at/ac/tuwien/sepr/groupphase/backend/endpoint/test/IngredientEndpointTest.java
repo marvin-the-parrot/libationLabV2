17dc369a7ab49a0f2e8073f8831a419fb431291f
Katarina Pjanic <e1549616@student.tuwien.ac.at>
2023-12-24 03:19:28 +0100

    Revert "Build fix"
    This reverts commit a843b31c9fbab0ff5791ca91f5f7c94fa00ef783.

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/test/IngredientEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/test/IngredientEndpointTest.java
@@ -77,16 +77,16 @@ public class IngredientEndpointTest {
     @Test
     @WithMockUser(username = "user1@email.com")
     public void getIngredientSuggestions_getSuggestionsForGroup1_expectedSuccess() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/ingredients/suggestions/{groupId}", 1)).andExpect(status().isConflict()).andReturn();
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/ingredients/suggestions/{groupId}", 1)).andExpect(status().isOk()).andReturn();
         var contentResult = mvcResult.getResponse().getContentAsString();
-        assertFalse(contentResult.contains("Lime"));
-        assertFalse(contentResult.contains("Sweet Vermouth"));
-        assertFalse(contentResult.contains("Triple sec"));
-        assertFalse(contentResult.contains("Tequila"));
-        assertFalse(contentResult.contains("Creme de Cassis"));
-        assertFalse(contentResult.contains("Green Chartreuse"));
-        assertFalse(contentResult.contains("Orange"));
-        assertFalse(contentResult.contains("Grenadine"));
+        assertTrue(contentResult.contains("Lime"));
+        assertTrue(contentResult.contains("Sweet Vermouth"));
+        assertTrue(contentResult.contains("Triple sec"));
+        assertTrue(contentResult.contains("Tequila"));
+        assertTrue(contentResult.contains("Creme de Cassis"));
+        assertTrue(contentResult.contains("Green Chartreuse"));
+        assertTrue(contentResult.contains("Orange"));
+        assertTrue(contentResult.contains("Grenadine"));
     }
 
     @Test
