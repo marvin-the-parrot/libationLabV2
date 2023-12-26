73fc3a79737d4b238dfce4448736d80cf1f0a3ba
Katarina Pjanic <e1549616@student.tuwien.ac.at>
2023-12-26 19:01:23 +0100

    #57 Added display of selected menu cocktails in group page

--- frontend/src/app/components/groups/groups.component.ts
+++ frontend/src/app/components/groups/groups.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {GroupOverview} from "../../dtos/group-overview";
 import {GroupsService} from "../../services/groups.service";
+import {MenuCocktailsDto} from 'src/app/dtos/menu';
+import {CocktailService} from 'src/app/services/cocktail.service';
+import {Observable, of} from 'rxjs';
+import {map, catchError} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-groups',
@@ -17,6 +22,7 @@ export class GroupsComponent implements OnInit {
   constructor(
     public authService: AuthService,
     public groupService: GroupsService,
+    private cocktailService: CocktailService,
   ) {
   }
 
@@ -28,8 +34,15 @@ export class GroupsComponent implements OnInit {
     this.groupService.getAllByUser().subscribe({
       next: (groups: GroupOverview[]) => {
         for (let group of groups) {
-          if (group.cocktails==null){
-            group.cocktails=["Mojito", "Manhatten", "Old Fashioned", "B52"];
+          if (group.cocktails == null) {
+            this.getCocktailsMenu(group.id).subscribe({
+              next: (cocktails: string[]) => {
+                group.cocktails = cocktails;
+              },
+              error: error => {
+                console.error(`Error fetching cocktails for group ${group.id}`, error);
+              }
+            });
           }
         }
         this.groups = groups;
@@ -38,7 +51,17 @@ export class GroupsComponent implements OnInit {
         console.error('Error fetching groups', error);
         // Handle error appropriately (e.g., show a message to the user)
       }
-  });
+    });
   }
-
+  
+  private getCocktailsMenu(groupId: number): Observable<string[]> {
+    return this.cocktailService.getCocktailMenu(groupId).pipe(
+      map((menu: MenuCocktailsDto) => menu.cocktailsList.map(cocktail => cocktail.name)),
+      catchError(error => {
+        console.error(`Error fetching cocktail menu for group ${groupId}`, error);
+        return of([]);
+      })
+    );
+  }
+  
 }
